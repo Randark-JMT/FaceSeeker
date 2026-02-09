cdafcea62b48a575128ef5ff280b9c40f49e49a3
Randark <randark@randark.site>
2026-02-09 15:08:27 +0800

    启用 WAL 日志模式以提高数据库性能

--- core/database.py
+++ core/database.py
@@ -19,6 +19,7 @@ class DatabaseManager:
         self.conn = sqlite3.connect(self.db_path, check_same_thread=False)
         self.conn.row_factory = sqlite3.Row
         self.conn.execute("PRAGMA foreign_keys = ON")
+        self.conn.execute("PRAGMA journal_mode = WAL")
 
     def _init_db(self):
         """建表"""
